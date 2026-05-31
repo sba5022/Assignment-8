c97f4a96d6affd7e09dadc09e4aa6ee7cfe2995b
sba5022 <sumaiaofficial02@gmail.com>
2026-05-31 11:44:45 +0600

    better auth added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,35 +1,35 @@
 'use client';
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
-  const handleLoginFunc = (e) => {
-    e.preventDefault();
-    // Handle login logic here
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    console.log(email, password)
+   const { register,
+     handleSubmit,formState: { errors } }= useForm();
+     console.log(errors,'errors');
+  const handleLoginFunc = (data) => {
+   console.log(data,'data');
   };
 
     return (
         <div className='container mx-auto bg-slate-100 min-h-[80vh] flex items-center justify-center space-y-5'>
           <div className="p-4 rounded-md bg-white">
              <h2 className='text-lg font-bold'>Login</h2>
-        <form onSubmit={handleLoginFunc}> 
+        <form onSubmit={handleSubmit(handleLoginFunc)}> 
            <fieldset className="fieldset">
   <legend className="fieldset-legend">Email</legend>
   
   <input 
-  name="email"
+ {...register('email',{ required: true })}
   type="text" className="input" placeholder="Enter your Email" />
-  
+  {errors.email && <p className='text-sm text-red-500'>Email is required</p>}
 </fieldset>
 <fieldset className="fieldset">
   <legend className="fieldset-legend">Password</legend>
   <input 
-  name="password"
+  {...register('password',{ required: "password is required" })}
   type="text" className="input" placeholder="Enter your Password" />
-  
+  {errors.password && <p className='text-sm text-red-500'>{errors.password.message}</p>}
 </fieldset>
 <button className="btn btn-primary">Login</button></form>
 <p className="text-sm text-muted-foreground">
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,60 @@
+'use client';
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const RegisterPage = () => {
+   const { register,
+     handleSubmit,formState: { errors } }= useForm();
+   
+  const handleRegisterFunc = (data) => {
+   console.log(data,'data');
+  };
+
     return (
-        <div>
-            register
+        <div className='container mx-auto bg-slate-100 min-h-[80vh] flex items-center justify-center space-y-5'>
+          <div className="p-4 rounded-md bg-white">
+             <h2 className='text-lg font-bold'>Register</h2>
+        <form onSubmit={handleSubmit(handleRegisterFunc)}> 
+             <fieldset className="fieldset">
+  <legend className="fieldset-legend">Name</legend>
+  
+  <input 
+ {...register('name',{ required: true })}
+  type="text" className="input" placeholder="Enter your Name" />
+  {errors.name && <p className='text-sm text-red-500'>Name is required</p>}
+</fieldset>
+           <fieldset className="fieldset">
+  <legend className="fieldset-legend">Email</legend>
+  
+  <input 
+ {...register('email',{ required: true })}
+  type="text" className="input" placeholder="Enter your Email" />
+  {errors.email && <p className='text-sm text-red-500'>Email is required</p>}
+</fieldset>
+<fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input 
+  {...register('password',{ required: "password is required" })}
+  type="text" className="input" placeholder="Enter your Password" />
+  {errors.password && <p className='text-sm text-red-500'>{errors.password.message}</p>}
+</fieldset>
+ <fieldset className="fieldset">
+  <legend className="fieldset-legend">Photo URL</legend>
+  
+  <input 
+ {...register('photoURL',{ required: true })}
+  type="text" className="input" placeholder="Enter your Photo URL" />
+  {errors.photoURL && <p className='text-sm text-red-500'>Photo URL is required</p>}
+</fieldset>
+<button className="btn btn-primary">Register</button></form>
+<p className="text-sm text-muted-foreground">
+  Already have an account?{' '}
+  <Link href="/login" className="underline text-red-500">
+    Login
+  </Link>
+</p>
+          </div>
         </div>
     );
 };
